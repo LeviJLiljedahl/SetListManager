2443e9affba3e2d4ac719dc3a6b8d75fbd5f16c5
lmarquardt7 <lmarquardt7@student.cvtc.edu>
2024-04-19 17:41:40 -0500

    Initial Commit

---
+++ SetListManager/SetListManager_Form.cs
@@ -0,0 +1,10 @@
+namespace SetListManager
+{
+    public partial class SetListManager_Form : Form
+    {
+        public SetListManager_Form()
+        {
+            InitializeComponent();
+        }
+    }
+}
