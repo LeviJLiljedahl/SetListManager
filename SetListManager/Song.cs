7d33bbcffa7c0f5de95408aeb5f40c1ab772d7a3
lmarquardt7 <lmarquardt7@student.cvtc.edu>
2024-05-02 15:02:07 -0500

    finished current version of Song class

--- SetListManager/Song.cs
+++ SetListManager/Song.cs
@@ -12,5 +12,68 @@ namespace SetListManager
 {
     internal class Song
     {
+
+        // Class level Variables
+
+        private string cSongID;
+        private string cSongName;
+        private string cSongOriginalArtist;
+        private string cSongAsPerformedBy;
+        private string cSongLength;
+        private string cSongRating;
+
+        // Getters and Setters for class variables
+
+        public string SongID
+        { get { return cSongID; } set { cSongID = value; } }
+
+        public string SongName
+        { get { return cSongName; } set { cSongName = value; } }
+
+        public string SongOriginalArtist
+        { get { return cSongOriginalArtist; } set { cSongOriginalArtist = value; } }
+
+        public string SongAsPerformedBy
+        { get { return cSongAsPerformedBy; } set { cSongAsPerformedBy = value; } }
+
+        public string SongLength
+        { get { return cSongLength; } set { cSongLength = value; } }
+
+        public string SongRating
+        { get { return cSongRating; } set { cSongRating = value; } }
+
+        // Default Constructor
+
+        public Song()
+        {
+
+            cSongID = string.Empty;
+            cSongName = string.Empty;
+            cSongOriginalArtist = string.Empty;
+            cSongAsPerformedBy = string.Empty;
+            cSongLength = string.Empty;
+            cSongRating = string.Empty;
+
+        }
+
+        // Overloaded Constructor
+
+        public Song(string SongID)
+        {
+            cSongID = SongID;
+            cSongName = string.Empty;
+            cSongOriginalArtist = string.Empty;
+            cSongAsPerformedBy = string.Empty;
+            cSongLength = string.Empty;
+            cSongRating = string.Empty;
+        }
+
+        //Methods
+
+        // Override to string for listbox
+        public override string ToString()
+        {
+            return cSongID + " - " + cSongName;
+        }
     }
 }
